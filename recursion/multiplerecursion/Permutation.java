79080e43c72a5e6a2cb4b3c4a9f5a8b4f1ab91bc
bmoravineni <bmoravineni@paypal.com>
2022-12-22 20:01:42 +0530

    permutation - recusion by swapping approach

--- recursion/multiplerecursion/Permutation.java
+++ recursion/multiplerecursion/Permutation.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Permutation1 {
+public class Permutation {
 
   public static void main(String args[]) {
     
@@ -14,7 +14,13 @@ public class Permutation1 {
     System.out.println("Input: "+ Arrays.toString(arr));
     System.out.println("Output: ");
 
+    //recursion - approach of picking any element not picked before 
+    // time complexity: O(n!*n); space complexity: O(n); auxilary space complexity: O(n);
     printAllPermutations(arr, 0, new HashSet<>(), new ArrayList<>());
+
+    //recusion - approach of swapping
+    // time complexity: O(n!*n); space complexity: O(1); auxilary space complexity: O(n);
+    printAllPermutationsBySwapping(arr, 0);
   }
 
   public static void printAllPermutations(int[] arr, int low, Set<Integer> alreadyTaken, List<Integer> permutation) {
@@ -31,5 +37,25 @@ public class Permutation1 {
         alreadyTaken.remove(arr[i]);
       }
     }
+  }
+
+  public static void swap(int[] arr, int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  public static void printAllPermutationsBySwapping(int[] arr, int low) {
+    if(low>=arr.length) {
+      System.out.println(Arrays.toString(arr));
+      return;
+    }
+
+    for(int i=low;i<arr.length;i++) {
+      swap(arr, i, low);
+      printAllPermutationsBySwapping(arr, low+1);
+      swap(arr, i, low);
+    }
+    
   }
 }
